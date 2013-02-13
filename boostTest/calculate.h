df6b518e47773e111094185e824cfd7c3c4cdb5e
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-02-13 10:33:11 +0200

    added setup for testing boost

---
+++ boostTest/calculate.h
@@ -0,0 +1,21 @@
+/*
+ * calculate.h
+ *
+ *  Created on: Feb 11, 2013
+ *      Author: tapio
+ */
+
+#ifndef CALCULATE_H_
+#define CALCULATE_H_
+
+class Calculate {
+public:
+	Calculate();
+	~Calculate();
+	int sum(int &v1, int &v2);
+	int sub(int &v1, int &v2);
+	int multiply(int &v1, int &v2);
+};
+
+
+#endif /* CALCULATE_H_ */
